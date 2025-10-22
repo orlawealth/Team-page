399e1d15af728da9d98a4043a697638183fb5714
orlawealth <luv4junior@yahoo.com>
2025-10-22 14:03:59 +0100

    Add TeamMember component

---
+++ src/views/TeamView.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import TeamMember from '@/components/Teams/TeamMember.vue'
+import team from '@/team.json'
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <div>
+      <button
+        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+        :disabled="team.members.length == team.spots"
+      >
+        Add Member ({{team.spots - team.members.length}} spots Left)
+      </button>
+    </div>
+
+    <div>
+      <h3 class="inline--flex items-center text-3xl relative">
+        <img src="/smiley.png" alt="Img" class="mr-2" />
+        {{ team.name }} Team
+        <div
+          class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute right-4 -top-2"
+        >
+          {{ team.members.length }}
+        </div>
+      </h3>
+    </div>
+  </header>
+
+  <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
+    <table class="table-fixed border-spacing-2 border-separate">
+      <thead>
+        <th class="text-left px-6 py-2">Name</th>
+        <th class="text-left px-6 py-2">Email</th>
+        <th class="text-left px-6 py-2">Status</th>
+      </thead>
+
+      <tbody>
+        <TeamMember
+          v-for="member in team.members"
+          :name="member.name"
+          :email="member.email"
+          :status="member.status"
+        />
+      </tbody>
+    </table>
+
+    <p class="text-right text-gray-600 italic" v-show="team.members.length == team.spots">
+      There are no remaining team spots. Upgrade to add more.
+    </p>
+  </div>
+
+  <footer class="mt-12 bg-gray-100 py-4 text-center">
+    <h5 class="font-semibold text-lg">{{ team.name }} - {{ team.members.length }} Member Team</h5>
+  </footer>
+</template>
