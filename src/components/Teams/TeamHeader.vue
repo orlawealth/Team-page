a7fcd90ec8bf826602282e53364f20058080fd25
orlawealth <luv4junior@yahoo.com>
2025-10-22 15:13:34 +0100

    added pinia store for team

---
+++ src/components/Teams/TeamHeader.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useTeamStore } from '@/stores/TeamStore'
+
+let team = useTeamStore();
+
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <div>
+      <button
+        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+        :disabled="! team.spotsRemaining"
+      >
+        Add Member ({{team.spotsRemaining}} spots Left)
+      </button>
+    </div>
+
+    <div>
+      <h3 class="inline--flex items-center text-3xl relative">
+        <img src="/smiley.png" alt="Img" class="mr-2" />
+        {{ team.name }} Team
+        <div
+          class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute right-4 -top-2"
+        >
+          {{ team.spots }}
+        </div>
+      </h3>
+    </div>
+  </header>
+
+</template>
+
+<style scoped></style>
